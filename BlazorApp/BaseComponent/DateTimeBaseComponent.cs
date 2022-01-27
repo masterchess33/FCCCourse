4e850eead71647805c51ebdc1c07d4f6e49e9835
Pablo <pablo.luer@icloud.com>
2022-01-27 00:30:19 -0300

    Avance Curso Blazor.

---
+++ BlazorApp/BaseComponent/DateTimeBaseComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.BaseComponent;
+
+public class DateTimeBaseComponent : ComponentBase
+{
+
+    public DateTime dateTime { get; set; }
+}
